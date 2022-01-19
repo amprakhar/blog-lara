edcf533d98d36af8242e7acbbdb31e81f0cda30b
amprakhar <prakharsh@live.com>
2022-01-19 12:18:03 +0530

    updated posts table

--- database/migrations/2021_12_05_124210_create_posts_table.php
+++ database/migrations/2021_12_05_124210_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('status');
             $table->timestamps();
         });
     }
